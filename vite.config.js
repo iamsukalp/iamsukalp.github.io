35eb8d83350820ae0157fbbc2f88ed2304a92136
learntripathi <learntripathi@gmail.com>
2024-07-26 23:48:04 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     mimeTypes: {
       'application/javascript': ['js'],
